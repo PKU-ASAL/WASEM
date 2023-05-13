6080ff5a9b5d2c789c4a569ade4440132f704afd
qiangwuyinhe <1700013015@pku.edu.cn>
2023-05-13 17:54:28 +0000

    origin

--- main.py
+++ main.py
@@ -15,51 +15,34 @@ from eunomia.arch.wasm.mythread import multi_thread_process
 def main():
 
     filename = sys.argv[1]
+    Ecall_list = sys.argv[2].split(",")
 
-    #Configuration.set_verbose_flag(args.verbose)
-    #Configuration.set_file(sys.argv[1])
-    #Configuration.set_entry(args.onlyfunc)
-    #Configuration.set_coverage(args.coverage)
-    #Configuration.set_visualize(args.visualize)
-    #Configuration.set_lasers(args.overflow, args.divzero, args.buffer)
-    #Configuration.set_source_type(args.source_type)
-    #Configuration.set_algo(args.algo)
-    #Configuration.set_concrete_globals(args.concrete_globals)
-    #Configuration.set_solver(args.solver)
-    #Configuration.set_stdin(args.stdin, args.sym_stdin)
-    #Configuration.set_sym_files(args.sym_files)
-    #Configuration.set_incremental_solving(args.incremental)
     with open(filename,'rb') as f:
         octo_bytecode = f.read()
     f.close()
 
-
-
-    watfile = filename[:-5] + ".wat"
-    namelist = []
-    with open(watfile,'r') as wf:
-        while True:
-            line = wf.readline()
-            if line == "":
-                break
-            if line[0:9] == "  (func $":
-                start = 9
-                end = 10
-                while line[end] != ' ':
-                    end += 1
-                name = line[start:end]
-                namelist.append(name)
-    wf.close()
-
-
-
-
+    if len(sys.argv) == 3:
+        namelist = []
+        watfile = filename[:-5] + ".wat"
+        with open(watfile,'r') as wf:
+            while True:
+                line = wf.readline()
+                if line == "":
+                    break
+                if line[0:9] == "  (func $":
+                    start = 9
+                    end = 10
+                    while line[end] != ' ':
+                        end += 1
+                    name = line[start:end]
+                    namelist.append(name)
+        wf.close()
+    else:
+        namelist = sys.argv[3].split(",")
+    
     # import necessary part
     from eunomia.arch.wasm.emulator import WasmSSAEmulatorEngine
 
-
-    #Ecall_list = ['sgx_ecall_create_wallet', 'sgx_ecall_show_wallet', 'sgx_ecall_change_master_password', 'sgx_ecall_add_item', 'sgx_ecall_remove_item']
-    Ecall_list = sys.argv[2].split(",")
     multi_thread_process(octo_bytecode, namelist, Ecall_list)
 
 
